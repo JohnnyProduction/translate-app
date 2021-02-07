bf912983a5db0295cc7ba3964a10f5422810cad6
JohnnyProduction <johnnyproduction@yandex.ru>
2021-02-08 01:16:23 +0300

    fix css & add languages

--- src/app/app.humanize-lang.pipe.ts
+++ src/app/app.humanize-lang.pipe.ts
@@ -7,8 +7,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AppHumanizeLangPipe implements PipeTransform {
   transform(value: string): string {
     switch (value) {
-      case 'ru': return 'Русский'
-      case 'en': return 'Английский'
+      case 'ru': return 'Русский';
+      case 'en': return 'Английский';
+      case 'fr': return 'Французский';
+      case 'de': return 'Немецкий';
+      case 'es': return 'Испанский';
       default: return  'Неизвестный язык'
     }
   }
